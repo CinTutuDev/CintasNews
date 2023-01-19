57a6d0f68712063df17ecea3f5ecf9a91ae971cd
cinta <cintatafur@gmail.com>
2023-01-19 18:04:41 +0100

    ♻️ Fix(refactor):segmento cada noticia tab2

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/interfaces';
+import { ApiNewsService } from 'src/app/services/api-news.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
   public cagories: string[] = [
     'business',
     'entertainment',
@@ -14,11 +16,25 @@ export class Tab2Page {
     'technology',
   ];
   //Se hace para seleccionar la 1º posicion
-  public selectedCategory: string = this.cagories[0]
+  public selectedCategory: string = this.cagories[0];
+  public articles: Article[] = [];
 
-  constructor() {}
+  constructor(private apiNewsService: ApiNewsService) {}
 
-  segmentChanged(category: any) {
-    console.log('Segment changed', category);
+  ngOnInit(): void {
+    this.apiNewsService
+      .getTopHeadLinesByCategory(this.selectedCategory)
+      .subscribe((articles) => {
+        this.articles = [...this.articles, ...articles];
+      });
+  }
+
+  segmentChanged(event: any) {
+    this.selectedCategory = event.detail.value;
+    this.apiNewsService
+      .getTopHeadLinesByCategory(this.selectedCategory)
+      .subscribe((articles) => {
+        this.articles = [...articles];
+      });
   }
 }
--- src/app/services/api-news.service.ts
+++ src/app/services/api-news.service.ts
@@ -26,4 +26,16 @@ export class ApiNewsService {
       )
       .pipe(map(({ articles }) => articles));
   }
+  getTopHeadLinesByCategory(category: string): Observable<Article[]> {
+    return this.http
+      .get<NewsResponse>(
+        `https://newsapi.org/v2/top-headlines?country=us&category=${category}`,
+        {
+          params: {
+            apiKey,
+          },
+        }
+      )
+      .pipe(map(({ articles }) => articles));
+  }
 }
