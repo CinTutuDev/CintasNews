486753694cfa16717384ce56d674de800cffc619
cinta <cintatafur@gmail.com>
2023-01-19 17:06:36 +0100

    💄 ✅Add(segment/style):anulado scroll en scss

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -3,10 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  public cagories: string[] = [
+    'business',
+    'entertainment',
+    'general',
+    'sports',
+    'technology',
+  ];
+  //Se hace para seleccionar la 1º posicion
+  public selectedCategory: string = this.cagories[0]
 
   constructor() {}
 
+  segmentChanged(category: any) {
+    console.log('Segment changed', category);
+  }
 }
