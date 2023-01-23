54f7b2b919fcc2b75f8e20117baf591d87cad087
cinta <cintatafur@gmail.com>
2023-01-23 16:49:35 +0100

    🐛 fix(InAppBrowser):solucion bug y agrego pluggins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,22 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 
+//Pluggins
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule,
-           IonicModule.forRoot(),
-           AppRoutingModule,
-           HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    InAppBrowser,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Article } from '../../interfaces';
-
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { Platform } from '@ionic/angular';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss'],
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent {
   @Input()
   article!: Article;
   @Input()
   index!: number;
 
-  constructor() {}
+  constructor(private iab: InAppBrowser, private platform: Platform) {}
 
-  ngOnInit() {}
+  onClick() {}
+
+  openArticle() {
+    //si es en ios o android...
+    if (this.platform.is('ios') || this.platform.is('android')) {
+      const browser = this.iab.create(this.article.url, 'self');
+      browser.show();
+      return;
+    }
+    //si es navegador web
+    window.open(this.article.url, '_blank')
+  }
 }
